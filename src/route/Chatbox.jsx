03e12e486e74927b504bfed878b808fddc05af45
coolistkovbel <coolistkovbel@gmail.com>
2023-04-20 12:47:27 -0400

    kinda of eh, going to be setting up database, using it for messages and hopefully it will work well with socket.io feel like i wont really need socket.io if i setup a database but... we shall see i guess.

--- src/route/Chatbox.jsx
+++ src/route/Chatbox.jsx
@@ -1,4 +1,5 @@
 import {useEffect} from 'react'
+import io from 'socket.io-client';
 import { useLoaderData } from "react-router-dom"
 import { getChannelById, mintAccess } from "../deApp"
 
@@ -13,16 +14,42 @@ export async function loader({params}) {
   }
 }
 
+let socket;
 
 
 export default function Chatbox() {
   const {joined, channel} = useLoaderData()
+  const URL = 'localhost:4266'
+
+
+  useEffect(() => {
+
+    socket = io(URL)
+
+    console.log(socket)
+    console.log(channel.name)
+    console.log(channel.id.toString())
+
+
+    socket.emit('join', {name: channel.name, room: channel.id.toString()}, () => {
+      
+    })
+
+    return () => {
+      // socket.emit('disconnect');
+      socket.off()
+    }
+
+  },[joined, channel])
+
+
+
 
 
   return (
     <>
       <div id="chatbox">
-        <h2>Chat Channel</h2>
+        <h2>{channel.name} Channel</h2>
         <div className="chat">
           {
             !joined ? (
