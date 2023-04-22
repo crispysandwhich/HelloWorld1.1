efe95b3b2ac642f8647d92fd90000035beb2cc6f
coolistkovbel <coolistkovbel@gmail.com>
2023-04-22 19:09:57 -0400

    a little bit of frontend for now... kinda sad

--- src/route/intro.jsx
+++ src/route/intro.jsx
@@ -1,46 +1,38 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-
-import Rose from '../assets/images/Rose.png'
-
+import Rose from "../assets/images/Rose.png";
+import "../assets/css/intro.css";
 
 function Intro() {
-
-
   return (
-    <section className='introSection'>
+    <section className="introSection">
+      <div className="introSection__content">
+        <header className="introSection__header">
+          <h2 className="app-title">$ad Inside</h2>
+          <h3 className="app-realeased-date"> May 2020 </h3>
+        </header>
 
-    <div className="introSection__content">
-      
-      <header className="introSection__header">
-        <h2 className="app-title">$ad Inside</h2>
-        <h3 className="app-realeased-date"> May 2020 </h3>
-      </header>
+        <div className="app-content">
+          <h1 className="glitch">
+            <Link to={"/app"}>&#91; What Is Life? &#93;</Link>
+          </h1>
+          <h1 className="glitch">
+            <Link to={"/app"}>&#91; What Is Life? &#93;</Link>
+          </h1>
+          <h1 className="glitch">
+            <Link to={"/app"}>&#91; What Is Life? &#93;</Link>
+          </h1>
+        </div>
 
-      <div className="app-content">
-
-        <p className='glitch'>
-          <Link to={"/app"}>
-            <span aria-hidden="true">&#91; What Is Life? &#93;</span>
-            <span>&#91; What Is Life? &#93;</span>
-            <span aria-hidden="true">&#91; What Is Life? &#93;</span>
-          </Link>
-        </p>
-
-      </div>
-
-      <div className="app-bottom">
-        <h3>Momento Mori </h3>
-        <div className="app-img-container">
-          <img src={Rose} alt="life is like a rose, beutiful but hurts" />
+        <div className="app-bottom">
+          <h3>Momento Mori </h3>
+          <div className="app-img-container">
+            <img src={Rose} alt="life is like a rose, beutiful but hurts" />
+          </div>
         </div>
       </div>
-
-    </div>
-
-
     </section>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
